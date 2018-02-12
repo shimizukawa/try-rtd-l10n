8865d8ec37b213bf77f97f703a1067fbfdb102ef
shimizukawa <shimizukawa@gmail.com>
2018-02-12 15:56:22 +0900

    fix nits

--- release.sh
+++ release.sh
@@ -7,17 +7,14 @@ RELEASE=1_7
 # setup environment
 SPHINXINTL_TRANSIFEX_USERNAME=sphinxjp
 SPHINXINTL_TRANSIFEX_PROJECT_NAME=sphinx-doc-${RELEASE}
-git submodule init
-git submodule update
-(cd sphinx; git fetch origin)
 find sphinx -name "*.pyc" -exec rm {} \;
 git checkout master
+git submodule init
+git submodule update
+# checkout sphinx master
+(cd sphinx; git fetch origin; git checkout master)
 pip install -r requirements.txt
 
-# setup environment
-cd sphinx
-git checkout master  # checkout sphinx master
-cd ../
 
 # update transifex pot and local po files
 sh ./locale/update.sh
